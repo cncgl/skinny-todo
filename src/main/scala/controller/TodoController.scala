9cecfc554d6e73b3be750d4652751cc20dd0a258
Shigeru Kondou <concigel@gmail.com>
2015-09-27 02:45:48 +0900

    Added Todo class

--- src/main/scala/controller/TodoController.scala
+++ src/main/scala/controller/TodoController.scala
@@ -1,15 +1,23 @@
 package controller
 
 import skinny.SkinnyController
+import skinny.util.JSONStringOps._
 
 /**
+ * Todo を扱うコントローラー
  * Created by shigeru on 15/09/26.
  */
 class TodoController extends SkinnyController {
   protectFromForgery()    // CSRF protection enabled
 
+  before() {
+    contentType = formats("json")
+  }
+
   def index = {
-    render("index")
+    val todo = Todo(status = true, title = "meeting")
+    toJSONString(todo)
+    //render("index")
   }
 
   def show = {
@@ -29,3 +37,5 @@ class TodoController extends SkinnyController {
   }
 
 }
+
+case class Todo(status: Boolean = false, title: String)
