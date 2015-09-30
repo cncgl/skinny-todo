99f5efe31cef73c91887a2a869c4ae6f71229d28
Shigeru Kondou <concigel@gmail.com>
2015-09-30 23:48:19 +0900

    Added keystore

--- src/main/scala/model/Todos.scala
+++ src/main/scala/model/Todos.scala
@@ -11,12 +11,15 @@ import skinny.orm.SkinnyCRUDMapper
  */
 case class Todos(id: Long, status: Boolean, title: String, inserted_at: DateTime, updated_at: DateTime)
 object Todos extends SkinnyCRUDMapper[Todos] {
+  override val columns = Seq("id", "status", "title", "inserted_at", "updated_at")
   override def defaultAlias = createAlias("m")
   override def extract(rs: WrappedResultSet, n: ResultName[Todos]): Todos = new Todos(
-    id          = rs.get(n.id),
-    status      = rs.get(n.status),
-    title       = rs.get(n.title),
-    inserted_at = rs.get(n.inserted_at),
-    updated_at  = rs.get(n.updated_at)
+    // Type Dynamic による取得はできるが、列名を指定すると取得できない
+    // IDE などでは対応していないが、コンパイルは scala 2.10 以降ならビルドできる。
+    id          = rs.long(n.id),
+    status      = rs.boolean(n.status),
+    title       = rs.string(n.title),
+    inserted_at = rs.jodaDateTime(n.inserted_at),
+    updated_at  = rs.jodaDateTime(n.updated_at)
   )
 }
